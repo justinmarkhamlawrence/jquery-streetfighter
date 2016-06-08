a8c0703520ffad8c2b7a4d4fa91eb02eff025c6c
Justin Lawrence <justinmarkhamlawrence@gmail.com>
2016-06-08 09:36:56 -0400

    js mousedown

--- js/app.js
+++ js/app.js
@@ -6,5 +6,17 @@ $(document).ready(function() {
 	$('.ryu').mouseleave(function() {
 		$('.ryu-ready').hide();
 		$('.ryu-still').show();
-	});
+	})
+	.mousedown(function() {
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').show();
+    // play hadouken sound
+    // show hadouken and animate it to the right of the screen
+ 	})
+  	.mouseup(function() {
+    	$('.ryu-throwing').hide();
+    	$('.ryu-ready').show();
+    // ryu goes back to his ready position
+    });
 });
